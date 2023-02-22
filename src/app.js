89d343a2d463b52acf131fca08d5fcdf48109f96
Johan <johanarley352@gmail.com>
2023-02-22 15:13:17 -0500

    Se agrego app.js, y el servidor

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import  express  from "express";
-
+import dotenv from 'dotenv';
+dotenv.config({path: '.env'})
 
 let app = express();
 app.use(express.json());
 
-app.listen(3001)
+
+const port = process.env.LOCALSERVERT
+
+app.listen(port , () => {
+    console.log(`Servidor listening on ${port}`)
+})
